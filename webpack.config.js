90150f3793c1099e7fceea8eddc8ab9f042a0e04
wuxuweilalala <2509165479@qq.com>
2021-01-08 20:26:28 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+
+
+module.exports = {
+  mode:process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry:'./src/index.tsx',
+  output:{
+    path:path.join(__dirname,'src'),
+    filename:'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer:{
+    hot:true,
+    contentBase:path.join(__dirname,'src'),
+    historyApiFallback:{ // browserHistory 的时候，刷新会刷 404
+      index:'./index.html'
+    }
+  },
+  resolve:{
+    alias :{
+      "@":path.resolve(__dirname,'src'),
+      "~":path.resolve(__dirname,'node_modules')
+    },
+    extensions:['.ts','.tsx','.js','.json']
+  },
+  module:{
+    rules:[
+      {
+        test: /\.(j|t)sx?$/,
+        loader:'ts-loader',
+        exclude:/node_modules/
+      },
+      {
+        enforce:'pre',
+        test: /\.(j|t)sx?$/,
+        loader:'source-map-loader'
+      },
+      {
+        test: /\.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test: /\.less$/,
+        use:['style-loader','css-loader','less-loader']
+      },
+      {
+        test: /\.(jpg|png|gif|svg|jpeg)$/,
+        use:['url-loader']
+      },
+    ]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
